c4b1b5a48ba7c92c5aeeb29f2e6433d52c844629
Jurgens Smit <contact@jurgenssmit.com>
2013-04-16 19:54:12 +1000

    fixed logout link

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Treebook::Application.routes.draw do
     devise_scope :user do
     get "register", to: "devise/registrations#new", as: :register
     get "login", to: "devise/sessions#new", as: :login
-    get "logout", to: "devise/sessions#destoy", as: :logout
+    get "logout", to: "devise/sessions#destroy", as: :logout
   end
 
   resources :statuses
