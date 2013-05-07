25859b1c5bfebb3ca0d3bc330ad227d3d4c550c9
Jurgens Smit <contact@jurgenssmit.com>
2013-05-07 19:54:02 +1000

    fix user status select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-	def flash_class (type)
+
+	def flash_class(type)
 		case type
 		when :alert
 			"alert-error"
 		when :notice
-			"alert-sucess"
+			"alert-success"
 		else
 			""
 		end
+	end
 end
-end
-
